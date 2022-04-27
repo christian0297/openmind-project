cc7728a1a0b8f1d8dc1f5d6d7fd06e4b7024f4ce
Christian Marfia <christianmarfia@hotmail.com>
2022-04-28 00:51:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector(".banner-close").addEventListener("click", ()=>{
+    document.querySelector(".banner").style.display = "none";
+    //document.querySelector(".banner").remove();
+    //vedere quale dei due è migliore dopo aver visto lezione sul caching
+});
